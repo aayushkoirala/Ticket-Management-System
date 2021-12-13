4b9445459899d20de2df353ab79142ee2d2be846
Aayush <koiralaaayush1@gmail.com>
2021-12-13 15:38:15 -0800

    fixed folder issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import SignUp from './components/user_auth/SignUp';
-import LogIn from "./components/user_auth/LogIn";
+import SignUp from './components/auth/SignUp';
+import LogIn from './components/auth/LogIn';
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
